8a03fb687d047b46bd0bf310dec0db2f6f1bce88
zaidlhbot2 <102425946+zaidlhbot2@users.noreply.github.com>
2022-09-03 15:49:03 +0100

    Update config.js

--- system/config.js
+++ system/config.js
@@ -1,7 +1,7 @@
 // Owner number
-global.owner = '212632244668'
+global.owner = '212639319505'
 // Owner name
-global.owner_name = 'M O H A M E D    B E L L A'
+global.owner_name = '╰┈⫸🆉🅰🅸🅳 🅻🅷⫷┈┈╯'
 // Maximum upload file size limit (Default : 250 MB)
 global.max_upload = 250
 // Delay for spamming protection (Default : 3 seconds)
@@ -15,14 +15,14 @@ global.evaluate_chars = ['=>', '~>', '<', '>', '$']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
 global.blocks = ['91', '92']
 // Put target jid to forward friends story
-global.forwards = '212692516633@c.us'
+global.forwards = '212639319505@c.us'
 // Get neoxr apikey by registering at https://api.nxr.my.id
 global.Api = new (require('./neoxrApi'))('dKwtXX')
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
 global.chatai_bid = '164728'
 global.chatai_key = 'MKPsfkgXLZPGrWoH'
 // Footer text
-global.footer = 'B O T   BY  M O H A M E D   B E L L A'
+global.footer = 'B O T   BY  ╰┈⫸🆉🅰🅸🅳 🅻🅷⫷┈┈╯'
 // Global status
 global.status = Object.freeze({
    wait: Func.texted('bold', 'المرجو الانتطار . . .'),
@@ -35,7 +35,7 @@ global.status = Object.freeze({
    premium: Func.texted('bold', 'This feature only for premium user.'),
    owner: Func.texted('bold', 'This command only for owner.'),
    god: Func.texted('bold', 'This command only for Master'),
-   group: Func.texted('bold', 'غير مسموح لهذا الحساب باستخدام البوت ٠ تواصل مع صاحب البوت من اجل الانضمام للمجموعة wa.me/212690516633'),
+   group: Func.texted('bold', 'غير مسموح لهذا الحساب باستخدام البوت ٠ تواصل مع صاحب البوت من اجل الانضمام للمجموعة wa.me/212639319505'),
    botAdmin: Func.texted('bold', 'This command will work when I become an admin.'),
    admin: Func.texted('bold', 'This command only for group admin.'),
    private: Func.texted('bold', 'Use this command in private chat.')
